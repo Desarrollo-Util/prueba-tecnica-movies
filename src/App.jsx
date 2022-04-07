43cef792b682be0b8fa6fd7dc87c19e62ed882e3
Josyto <josyto7@gmail.com>
2022-04-07 20:51:40 +0200

    Movie card and movie DTO

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import MovieCard from './components/movie-card';
 import { useMoviesSearch } from './lib/hooks/use-movies-search';
 
 const App = () => {
@@ -9,7 +10,18 @@ const App = () => {
 
 	return (
 		<div>
-			{movies && movies.map(movie => <div key={movie.id}>{movie.title}</div>)}
+			<div className='container container-xl mx-auto flex flex-wrap'>
+				{movies &&
+					movies.map(movie => (
+						<MovieCard
+							key={movie.id}
+							title={movie.title}
+							image={movie.image}
+							year={movie.year}
+							rating={movie.rating}
+						/>
+					))}
+			</div>
 			<button onClick={() => setPage(page + 1)}>Página: {page}</button>
 		</div>
 	);
--- src/constants/api.js
+++ src/constants/api.js
@@ -1,1 +1,2 @@
 export const API_HOST = 'https://api.themoviedb.org/3';
+export const API_IMAGE_HOST = 'https://image.tmdb.org/t/p/original';
