dffe1895e769156e27d921f3d231ee75990569dd
Josyto <josyto7@gmail.com>
2022-04-21 10:39:58 +0200

    🔧 ⬆️ Eslint package and config updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,6 @@ module.exports = {
 		browser: true,
 		es2021: true
 	},
-	settings: {
-		react: {
-			version: 'detect'
-		}
-	},
 	extends: [
 		'plugin:react/recommended',
 		'plugin:react/jsx-runtime',
