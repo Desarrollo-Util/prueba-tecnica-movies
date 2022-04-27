d1eee102cd5df38e2bdbb20b460420813663ec0c
Josyto <josyto7@gmail.com>
2022-04-27 20:14:30 +0200

    Simple proxy to TMDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,23 @@
-console.log('HOLA MUNDO');
+const express = require('express');
+const dotenv = require('dotenv');
+const fetch = require('node-fetch');
+dotenv.config();
+
+const expressApp = express();
+
+expressApp.get('/peliculas', async (req, res) => {
+	const response = await fetch(
+		`https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.TMDB_API_KEY}&page=1`,
+		{
+			method: 'GET'
+		}
+	);
+
+	const data = await response.json();
+
+	res.send(data);
+});
+
+expressApp.listen(process.env.PORT, () =>
+	console.log(`Servidor de Express levantado en el puerto ${process.env.PORT}`)
+);
