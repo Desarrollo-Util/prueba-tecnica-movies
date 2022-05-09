d34050fdde29b4229926d1f7712392b50196adaa
Josyto <josyto7@gmail.com>
2022-05-09 19:35:06 +0200

    Dirty code with dirty tests
    Co-authored-by: Pablo Castellanos <pablocdev@users.noreply.github.com>

---
+++ backend/src/app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const { test: validateUuid } = require('uuid-random');
+dotenv.config();
+
+const expressApp = express();
+
+// Middleware
+expressApp.use(express.json());
+
+// Memory BBDD
+const users = [];
+
+// Regex validations
+const emailRegex =
+	/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const nameRegex = /^([A-Z\-\s\u00e1\u00e9\u00ed\u00f3\u00fa\u00f1]){2,30}$/i;
+
+// Endpoints
+expressApp.post('/register', (req, res) => {
+	const { body } = req;
+	const { id, name, email, password } = body;
+
+	// Validar los campos
+	if (!id || typeof id !== 'string' || !validateUuid(id))
+		return res.status(400).send('El formato del id no es correcto');
+	if (!email || typeof email !== 'string' || !emailRegex.test(email))
+		return res.status(400).send('El formato del email no es correcto');
+	if (!name || typeof name !== 'string' || !nameRegex.test(name))
+		return res.status(400).send('El formato del nombre no es correcto');
+	if (
+		!password ||
+		typeof password !== 'string' ||
+		password.length < 8 ||
+		password.length > 20
+	)
+		return res.status(400).send('El formato de la contraseña no es correcto');
+
+	// Validar que el usuario es único en persistencia (id, email)
+	const existingUser = users.find(
+		user => user.id === id || user.email === email
+	);
+	if (existingUser)
+		return res.status(409).send('El usuario ya se encuentra registrado');
+
+	users.push({
+		id,
+		name,
+		email,
+		password
+	});
+
+	console.log(users);
+	return res.send('El usuario se ha registrado de forma correcta');
+});
+
+module.exports = expressApp;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,5 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const fetch = require('node-fetch');
-dotenv.config();
+const app = require('./app');
 
-const expressApp = express();
-
-expressApp.get('/peliculas', async (req, res) => {
-	const response = await fetch(
-		`https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.TMDB_API_KEY}&page=1`,
-		{
-			method: 'GET'
-		}
-	);
-
-	const data = await response.json();
-
-	res.send(data);
-});
-
-expressApp.listen(process.env.PORT, () =>
+app.listen(process.env.PORT, () =>
 	console.log(`Servidor de Express levantado en el puerto ${process.env.PORT}`)
 );
